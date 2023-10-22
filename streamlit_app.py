889d258a211966443bf9991b8f04bf2f18bff290
SURYA NARAYANAN K S <108388181+Surya152002@users.noreply.github.com>
2023-10-22 22:44:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,133 +1,160 @@
 import pandas as pd
-import streamlit as st
 import matplotlib.pyplot as plt
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from statsmodels.tsa.arima.model import ARIMA
+from sklearn.preprocessing import StandardScaler
+import streamlit as st
+import pickle
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 import nltk
-import io
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import os  # Added for file path handling
+import openai
 
-nltk.download('vader_lexicon')
 
-class SentimentAnalyzer:
-    def __init__(self):
-        self.sia = SentimentIntensityAnalyzer()
+openai.api_key = "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"
 
-    def transform_scale(self, score):
-        return 5 * score + 5  # Convert the sentiment score from -1 to 1 scale to 0 to 10 scale
+# Download the necessary NLTK datasets
+nltk.download('punkt')
+nltk.download('stopwords')
 
-    def calculate_overall_sentiment(self, reviews):
-        compound_scores = [self.sia.polarity_scores(review)["compound"] for review in reviews]
-        overall_sentiment = sum(compound_scores) / len(compound_scores)
-        return self.transform_scale(overall_sentiment)
+# Function to preprocess data
+def preprocess_data(df):
+    # Handle missing values by forward filling
+    df = df.fillna(method='ffill')
 
-    def analyze_sentiment(self, reviews):
-        sentiments = [{'compound': self.transform_scale(self.sia.polarity_scores(review)["compound"]),
-                       'pos': self.sia.polarity_scores(review)["pos"],
-                       'neu': self.sia.polarity_scores(review)["neu"],
-                       'neg': self.sia.polarity_scores(review)["neg"]}
-                      for review in reviews]
-        return sentiments
+    # Optionally, handle any remaining missing values by backward filling
+    df = df.fillna(method='bfill')
 
-    def analyze_periodic_sentiment(self, reviews, period):
-        period_reviews = [' '.join(reviews[i:i + period]) for i in range(0, len(reviews), period)]
-        return self.analyze_sentiment(period_reviews)
+    # Optionally, standardize (scale) the data to have zero mean and unit variance
+    scaler = StandardScaler()
+    df[df.columns] = scaler.fit_transform(df[df.columns])
 
-    def interpret_sentiment(self, sentiments):
-        avg_sentiment = sum([sentiment['compound'] for sentiment in sentiments]) / len(sentiments)
-        if avg_sentiment >= 6.5:
-            description = "Excellent progress, keep up the good work!"
-        elif avg_sentiment >= 6.2:
-            description = "Good progress, continue to work hard!"
+    return df
+
+def trading_advice(actual, prediction):
+    """
+    Provide trading advice based on prediction and actual prices.
+    """
+    if prediction > actual:
+        return "Based on the predictions, you might consider buying."
+    elif prediction < actual:
+        return "Based on the predictions, you might consider selling."
+    else:
+        return "The price seems stable. You might consider holding."
+
+def chatbot_response(user_input, predefined_responses):
+    # Tokenize and vectorize user input and predefined responses
+    vectorizer = TfidfVectorizer(tokenizer=lambda text: nltk.word_tokenize(text, language='english'),
+                                 stop_words=stopwords.words('english'))
+    vectors = vectorizer.fit_transform([user_input] + predefined_responses)
+
+    # Calculate cosine similarities
+    cosine_matrix = cosine_similarity(vectors)
+
+    # Find the most similar predefined response to the user's input
+    response_idx = np.argmax(cosine_matrix[0][1:])
+
+    return predefined_responses[response_idx]
+
+def get_gpt3_response(user_input):
+    prompt = f"Answer questions related to cryptocurrency.\n\nUser: {user_input}\nBot:"
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=50  # You can adjust this based on response length
+    )
+    return response.choices[0].text
+
+
+# Streamlit app
+def main():
+    st.title("Cryptocurrency Price Prediction and Trading Bot")
+
+    # Specify the path to the CSV file
+    csv_path = '/content/crypto_dataset (9).csv'  # Update with your specific file path
+
+    # Check if the CSV file exists at the specified path
+    if not os.path.exists(csv_path):
+        st.error("The specified CSV file does not exist. Please provide a valid file path.")
+        return
+
+    # Read and preprocess the input data
+    input_data = pd.read_csv(csv_path, parse_dates=['Timestamp'], index_col='Timestamp')
+    input_data = preprocess_data(input_data)
+
+    # List of coins
+    coins = ['BTC-USD Close', 'ETH-USD Close', 'LTC-USD Close']
+
+    # Get user's choice of cryptocurrency
+    coin_choice = st.selectbox("Select a cryptocurrency", coins)
+
+    # Train ARIMA model if needed
+    if coin_choice == "BTC-USD Close":
+        coin_model_path = 'btc-usd_close_model.pkl'  # Update with your desired model path
+        coin_column = 'BTC-USD Close'
+    elif coin_choice == "ETH-USD Close":
+        coin_model_path = 'eth-usd_close_model.pkl'  # Update with your desired model path
+        coin_column = 'ETH-USD Close'
+    else:
+        coin_model_path = 'ltc-usd_close_model.pkl'  # Update with your desired model path
+        coin_column = 'LTC-USD Close'
+
+    # Check if the model exists, if not, train it
+    if not os.path.exists(coin_model_path):
+        # Split the data into training and testing sets
+        train_size = int(len(input_data) * 0.8)
+        train, test = input_data[:train_size], input_data[train_size:]
+
+        # Build and train the ARIMA model
+        model = ARIMA(train[coin_column], order=(5, 1, 0))  # Example order, you can tune this
+        model_fit = model.fit()
+
+        # Save the trained model using pickle
+        with open(coin_model_path, 'wb') as model_file:
+            pickle.dump(model_fit, model_file)
+
+    # Load the model using pickle
+    with open(coin_model_path, 'rb') as model_file:
+        model_fit = pickle.load(model_file)
+
+    # Make predictions
+    predictions = model_fit.forecast(steps=len(input_data))
+
+    # Display predictions
+    st.header("Price Predictions")
+    st.write(predictions)
+
+    # Visualize results
+    st.header("Price Prediction Chart")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(input_data.index, input_data[coin_column], label='Actual')
+    ax.plot(input_data.index, predictions, label='Predicted', linestyle='--')
+    ax.set_title(f'{coin_choice} Price Prediction')
+    ax.legend()
+    st.pyplot(fig)
+
+    # Chatbot live interaction
+
+    st.header("Chat with Trading Bot")
+
+    user_message = st.text_input("You: ")
+    predefined_responses = [
+        "The predicted price for the next period is {}.".format(predictions.iloc[-1]),
+        trading_advice(input_data[coin_column].iloc[-1], predictions.iloc[-1]),
+        "I am here to help with your cryptocurrency trading decisions.",
+        "Can you specify your query?"
+    ]
+
+    if user_message:
+        if "predict" in user_message.lower() or "forecast" in user_message.lower():
+            bot_reply = trading_advice(input_data[coin_column].iloc[-1], predictions.iloc[-1])
         else:
-            description = "Needs improvement, stay motivated and keep trying!"
+            bot_reply = get_gpt3_response(user_message)
+        st.write(f"Bot: {bot_reply}")
 
-        trend = "No change"
-        if len(sentiments) > 1:
-            first_half_avg = sum([sentiment['compound'] for sentiment in sentiments[:len(sentiments)//2]]) / (len(sentiments)//2)
-            second_half_avg = sum([sentiment['compound'] for sentiment in sentiments[len(sentiments)//2:]]) / (len(sentiments)//2)
-            if second_half_avg > first_half_avg:
-                trend = "Improving"
-            elif second_half_avg < first_half_avg:
-                trend = "Declining"
+if __name__ == "__main__":
+    main()
 
-        return description, trend
-
-
-# Streamlit UI
-st.title("Student Review Sentiment Analysis")
-
-# Upload CSV file
-csv_file = st.file_uploader("Upload your CSV file")
-
-if csv_file:
-    df = pd.read_csv(csv_file)
-    st.write(df.head())  # Debug statement to check the loaded data
-
-    students = df["Student"].tolist()
-    selected_student = st.selectbox("Select a student:", ["All Students"] + students)
-    review_period = st.selectbox("Review Period:", [1, 4])
-
-    if selected_student != "All Students":
-        student_data = df[df["Student"] == selected_student].squeeze()
-        st.write(student_data.head())  # Debug statement to check the filtered data
-
-        reviews = student_data[1:].tolist()
-        analyzer = SentimentAnalyzer()
-
-        if review_period == 1:
-            sentiments = analyzer.analyze_sentiment(reviews)
-        else:
-            sentiments = analyzer.analyze_periodic_sentiment(reviews, review_period)
-        st.write(sentiments)  # Debug statement to check the sentiments
-
-        overall_sentiment = analyzer.calculate_overall_sentiment(reviews)
-        st.subheader(f"Overall Sentiment for {selected_student}: {overall_sentiment:.2f}")
-        st.subheader("Sentiment Analysis")
-
-        # Plotting sentiment
-        weeks = list(range(1, len(sentiments) + 1))
-        sentiment_scores = [sentiment['compound'] for sentiment in sentiments]
-        pos_scores = [sentiment['pos'] for sentiment in sentiments]
-        neu_scores = [sentiment['neu'] for sentiment in sentiments]
-        neg_scores = [sentiment['neg'] for sentiment in sentiments]
-
-        fig, ax = plt.subplots()
-        ax.plot(weeks, sentiment_scores, label="Overall", color="blue")
-        ax.fill_between(weeks, sentiment_scores, color="blue", alpha=0.1)
-        ax.plot(weeks, pos_scores, label="Positive", color="green")
-        ax.plot(weeks, neu_scores, label="Neutral", color="gray")
-        ax.plot(weeks, neg_scores, label="Negative", color="red")
-
-        ax.set_xlabel('Week')
-        ax.set_ylabel('Sentiment Score')
-        ax.set_title(f'Sentiment Analysis for {selected_student}')
-        ax.legend()
-        st.pyplot(fig)
-
-        description, trend = analyzer.interpret_sentiment(sentiments)
-        st.subheader("Progress Description")
-        st.write(f"Sentiment Trend: {trend}")
-        st.write(f"Description: {description}")
-
-        # Breakdown of analysis
-        st.subheader("Breakdown of Analysis")
-        breakdown_df = pd.DataFrame(sentiments, index=list(range(1, len(sentiments) + 1)))
-        st.write(breakdown_df)
-
-        # Add a button to download student performance data
-        if st.button("Download Student Performance Data"):
-            output_df = pd.DataFrame({'Student': students})
-            output_df['Overall Sentiment'] = [analyzer.calculate_overall_sentiment(df[df["Student"] == student].squeeze()[1:].tolist()) for student in students]
-            output_df['Sentiment Description'] = [analyzer.interpret_sentiment(analyzer.analyze_sentiment(df[df["Student"] == student].squeeze()[1:].tolist()))[0] for student in students]
-
-            # Create a streamlit.io object to store the CSV data
-            output_data = io.StringIO()
-            output_df.to_csv(output_data, index=False)
-            output_data.seek(0)
-
-            # Display a download button for the CSV file
-            st.download_button(
-                label="Click here to download student performance data",
-                data=output_data.getvalue().encode('utf-8'),
-                file_name="student_performance.csv",
-                key='download_button'
-            )
